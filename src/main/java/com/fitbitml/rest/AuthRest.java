6371a39f3e27f67e2f3f48ff2caacf40a2549934
zinnour <zinnour@uqtr.ca>
2019-04-09 13:06:35 -0400

    commit 2

--- src/main/java/com/fitbitml/rest/AuthRest.java
+++ src/main/java/com/fitbitml/rest/AuthRest.java
@@ -60,7 +60,7 @@ public class AuthRest {
     @CrossOrigin(exposedHeaders="Access-Control-Allow-Origin")
     @RequestMapping(value = "/accessToken", method = RequestMethod.GET, produces = "application/json")
     public String getAccessToken() throws IOException {
-        return authService.getAccessToken();
+        return JSONObject.quote(authService.getAccessToken());
     }
 
     @CrossOrigin(exposedHeaders="Access-Control-Allow-Origin")
